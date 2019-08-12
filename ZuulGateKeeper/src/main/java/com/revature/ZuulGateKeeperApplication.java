b1213abbd1a22bb3b2130e7df332f2a80bcf26fe
Adam Kager <atksource>
2019-08-12 14:38:01 -0400

    GateKeeper without KeyMaster

--- ZuulGateKeeper/src/main/java/com/revature/ZuulGateKeeperApplication.java
+++ ZuulGateKeeper/src/main/java/com/revature/ZuulGateKeeperApplication.java
@@ -2,14 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaServer
+@EnableZuulProxy
 @SpringBootApplication
-public class DiscoveryApplication {
+public class ZuulGateKeeperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
+		SpringApplication.run(ZuulGateKeeperApplication.class, args);
 	}
 
 }
